c7fdbc79c9b045286e4e1bf259b25f31dfdcda9d
Udaykumar Bommala <udaykumarbommala@Udaykumars-MacBook-Pro.local>
2022-11-10 09:27:10 -0600

    Incorporated review comments

--- CMR-Metadata-Script/cmrmetadata.py
+++ CMR-Metadata-Script/cmrmetadata.py
@@ -32,12 +32,13 @@ page_num_limit = BaseConfig.PAGE_NUM_LIMIT
 with open(outfile, "w", newline= "") as f:
     writer = csv.writer(f,delimiter=",")
     writer.writerow(["Id", "Name", "ContentLength", "IngestionDate", "ContentDate:Start", "ContentDate:End", "Checksum:Algorithm", "Checksum:Value", "IsSealed", "SourceStorageId", "DestStorageId"])
+    count = 0
     while (page_num <= page_num_limit):
         formated_url = query_url.format(concept_id, page_num, page_size, date_range, bounding_box)
         print(formated_url)
         resp = requests.get(query_url.format(concept_id,page_num,page_size, date_range, bounding_box)).json()
         entries = resp["feed"]["entry"]
-        for i,granule in enumerate(entries):
+        for i,granule in enumerate  (entries):
             for link in granule["links"]:
                 if (link["href"].startswith(s3_bucket_name)):
                     Id = uuid.uuid1()
@@ -46,11 +47,13 @@ with open(outfile, "w", newline= "") as f:
                     ContentDateStart = datetime.fromisoformat(granule["time_start"][:-1]).astimezone(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
                     ContentDateEnd = datetime.fromisoformat(granule["time_end"][:-1]).astimezone(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
                     ContentLength = 100
-                    ChecksumAlgorithm = 'MD5'
+                    ChecksumAlgorithm = 'SHA-256'
                     ChecksumValue = '2c044996bfd029c30c22f62b51e19556'
                     IsSealed = "false"
                     SourceStorageId = "abcdef"
                     DestStorageId = "defabc"
                     writer.writerow([Id, Name, ContentLength, IngestionDate, ContentDateStart, ContentDateEnd, ChecksumAlgorithm,
                                     ChecksumValue, IsSealed, SourceStorageId, DestStorageId])
+                    count = count + 1
         page_num = page_num + 1
+    print("Total Number of records in CSV file is {}".format(count))
