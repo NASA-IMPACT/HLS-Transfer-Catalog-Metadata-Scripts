a8ef8f1bc48f13eb04af38560ef09c10798c3b97
Udaykumar Bommala <udaykumarbommala@Udaykumars-MacBook-Pro.local>
2022-11-17 15:57:20 -0600

    Incorporaetd review comments

--- CMR-Metadata-Script/cmrmetadata.py
+++ CMR-Metadata-Script/cmrmetadata.py
@@ -11,7 +11,7 @@ query_url = base_url + BaseConfig.GRANULES_URL
 filename = BaseConfig.OUTPUT_FILE_NAME
 extension = BaseConfig.OUTPUT_FILE_EXTENSION
 
-outfile = "cmrmetadata.csv"
+outfile = filename + extension
 
 concept_id = BaseConfig.CONCEPT_ID
 
@@ -29,6 +29,21 @@ bounding_box = BaseConfig.BOUNDING_BOX
 
 page_num_limit = BaseConfig.PAGE_NUM_LIMIT
 
+def writeToCsvFile(s3_bucket_name, granule):
+    Id = uuid.uuid1()
+    Name = link["href"].split(s3_bucket_name)[1]
+    IngestionDate = datetime.fromisoformat(granule["time_start"][:-1]).astimezone(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+    ContentDateStart = datetime.fromisoformat(granule["time_start"][:-1]).astimezone(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+    ContentDateEnd = datetime.fromisoformat(granule["time_end"][:-1]).astimezone(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+    ContentLength = 100
+    ChecksumAlgorithm = 'SHA-256'
+    ChecksumValue = '2c044996bfd029c30c22f62b51e19556'
+    IsSealed = "false"
+    SourceStorageId = "abcdef"
+    DestStorageId = "defabc"
+    writer.writerow([Id, Name, ContentLength, IngestionDate, ContentDateStart, ContentDateEnd, ChecksumAlgorithm,
+                    ChecksumValue, IsSealed, SourceStorageId, DestStorageId])
+
 with open(outfile, "w", newline= "") as f:
     writer = csv.writer(f,delimiter=",")
     writer.writerow(["Id", "Name", "ContentLength", "IngestionDate", "ContentDate:Start", "ContentDate:End", "Checksum:Algorithm", "Checksum:Value", "IsSealed", "SourceStorageId", "DestStorageId"])
@@ -36,24 +51,12 @@ with open(outfile, "w", newline= "") as f:
     while (page_num <= page_num_limit):
         formated_url = query_url.format(concept_id, page_num, page_size, date_range, bounding_box)
         print(formated_url)
-        resp = requests.get(query_url.format(concept_id,page_num,page_size, date_range, bounding_box)).json()
+        resp = requests.get(formated_url).json()
         entries = resp["feed"]["entry"]
         for i,granule in enumerate  (entries):
             for link in granule["links"]:
                 if (link["href"].startswith(s3_bucket_name)):
-                    Id = uuid.uuid1()
-                    Name = link["href"].split(s3_bucket_name)[1]
-                    IngestionDate = datetime.fromisoformat(granule["time_start"][:-1]).astimezone(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
-                    ContentDateStart = datetime.fromisoformat(granule["time_start"][:-1]).astimezone(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
-                    ContentDateEnd = datetime.fromisoformat(granule["time_end"][:-1]).astimezone(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
-                    ContentLength = 100
-                    ChecksumAlgorithm = 'SHA-256'
-                    ChecksumValue = '2c044996bfd029c30c22f62b51e19556'
-                    IsSealed = "false"
-                    SourceStorageId = "abcdef"
-                    DestStorageId = "defabc"
-                    writer.writerow([Id, Name, ContentLength, IngestionDate, ContentDateStart, ContentDateEnd, ChecksumAlgorithm,
-                                    ChecksumValue, IsSealed, SourceStorageId, DestStorageId])
+                    writeToCsvFile(s3_bucket_name,granule)
                     count = count + 1
         page_num = page_num + 1
     print("Total Number of records in CSV file is {}".format(count))
